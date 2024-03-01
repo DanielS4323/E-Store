2d36a57fda552d627ebb888dc7eb5f0a06cfa316
DanielS4323 <daky4323@gmail.com>
2024-03-01 17:00:12 +0100

    loading spinner

---
+++ src/components/UI/LoadingSpinner.tsx
@@ -0,0 +1,11 @@
+import { Loader } from 'semantic-ui-react';
+
+const LoadingSpinner = () => {
+  return (
+    <Loader size="huge" active>
+      Loading...
+    </Loader>
+  );
+};
+
+export default LoadingSpinner;
